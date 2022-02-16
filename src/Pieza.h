32ad0f35b24d7563329bc64a8ac24f6fce820164
ml.salto@alumnos.upm.es <ml.salto@alumnos.upm.es>
2022-02-16 12:23:17 +0100

    Agregar archivos de proyecto

--- src/Pieza.h
+++ src/Pieza.h
@@ -1,7 +1,7 @@
 #pragma once
 
-enum TipoPieza { REY, REINA, TORRE, ALFIL, CABALLO, PEON };
-enum ColorPieza { BLANCO, NEGRO };
+enum class TipoPieza { REY, REINA, TORRE, ALFIL, CABALLO, PEON, VACIO};
+enum class ColorPieza { BLANCO, NEGRO };
 
 class Pieza
 {
@@ -12,10 +12,10 @@ private:
 public:
 
 	void setColorPieza(ColorPieza x); //asigna el color blanco o negro
-	void setTipoPieza(TipoPieza x);
+	void setTipoPieza(TipoPieza x); //asigna el tipo de pieza (Rey, Reina...)
 
-	ColorPieza getColorPieza();
-	TipoPieza getTipoPieza();
+	ColorPieza getColorPieza(); //devuelve el color blanco o negro
+	TipoPieza getTipoPieza(); //devuelve el tipo de pieza (Rey, Reina...)
 
 };
 
