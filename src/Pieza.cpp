a15f90fe19edeace524e5477089398840fa75c16
ml.salto@alumnos.upm.es <ml.salto@alumnos.upm.es>
2022-02-16 14:12:26 +0100

    Agregar archivos de proyecto

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -10,6 +10,12 @@ void Pieza::setTipoPieza(TipoPieza x)
 	pieza = x;
 }
 
+void Pieza::setCasilla(Vector2D x)
+{
+	casilla = x;
+}
+
+
 ColorPieza Pieza::getColorPieza()
 {
 	return color;
@@ -20,3 +26,7 @@ TipoPieza Pieza::getTipoPieza()
 	return pieza;
 }
 
+Vector2D Pieza::getCasilla()
+{
+	return casilla;
+}
--- src/Pieza.h
+++ src/Pieza.h
@@ -12,13 +12,16 @@ private:
 	ColorPieza color;
 	TipoPieza pieza;
 
+	Vector2D casilla;
+
 public:
 
 	void setColorPieza(ColorPieza x); //asigna el color blanco o negro
 	void setTipoPieza(TipoPieza x); //asigna el tipo de pieza (Rey, Reina...)
+	void setCasilla(Vector2D x); //asigna en la casilla que se encuentra la pieza
 
 	ColorPieza getColorPieza(); //devuelve el color blanco o negro
 	TipoPieza getTipoPieza(); //devuelve el tipo de pieza (Rey, Reina...)
-
+	Vector2D getCasilla(); //devuelve la casilla en la que está la pieza
 };
 
