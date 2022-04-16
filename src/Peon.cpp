d89e213f75066653f0853b9530845b2ce65d8ace
gema-upm <g.ssanchez@alumnos.upm.es>
2022-04-16 19:19:59 +0200

    peon 2.0

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -35,44 +35,92 @@ bool Peon::movimientoLegal(Casilla* fin)
 		for (j = 0; j < 8; j++)
 			copia_casillas[i][j] = tablero.getCasillaT(i, j);
 
-	////// FILA Y COLUMNA //////
+	////// FILA Y COLUMNA ACTUAL DE LA PIEZA //////
 	row = (pos.x + 28) / 8;
 	coll = (pos.y + 28) / 8;
 
-	////// ALMACENAMIENTO DATOS DE ENTRADA //////
+	////// ALMACENAMIENTO DATOS DE ENTRADA QUE INTRODUCE EL USUARIO//////
 	int x_fin = fin->getColumna();
 	int y_fin = fin->getFila();
 
 	switch (color)
 	{
-	// los peones blancos solo se mueven hacia arriba y diagonales de arriba derecha/izquierda
+		// los peones blancos solo se mueven hacia arriba y diagonales de arriba derecha/izquierda en caso de haber una pieza negra
 	case 'B':
-		///////////       MOVIMIENTO ARRIBA    ///////////
-		if (row == x_fin && coll < 7)
+		if (coll == 1)
 		{
-			for (i = coll + 1; i <= y_fin && !obstaculo; i++)
+			////// PRIMER MOVIMIENTO A ESCOGER ENTRE 1-2 CASILLAS HACIA ARRIBA //////
+			if (y_fin <= coll + 2)
 			{
-				if (copia_casillas[x_fin][i]->getOcupada() == true) {
+				// Si hay una pieza en la casilla donde se quiere desplazar y no hay nada
+				if (copia_casillas[row][y_fin]->getOcupada() == false) {
 					obstaculo = true;
 					return false;
 				}
 				return true;
 			}
 		}
-		break;
-	//Los peones negros solo se mueven hacia abajo y diagonales de abajo derecha/izquierda
-	case 'N':
-		///////////       MOVIMIENTO ABAJO      ///////////
-		if (row == x_fin && coll > 0)
+		else
 		{
-			for (i = coll - 1; i >= y_fin && !obstaculo; i--)
+			////// RESTO DE MOVIMIENTOS 1 CASILLA HACIA ARRIBA /////
+			if (y_fin = coll + 1)
 			{
-				if (copia_casillas[x_fin][i]->getOcupada() == true) {
+				// Si hay una pieza en la casilla donde se quiere desplazar y no hay nada
+				if (copia_casillas[row][y_fin]->getOcupada() == false) {
 					obstaculo = true;
 					return false;
 				}
 				return true;
 			}
+		}
+
+		////// MOVIMIENTO DIAGONAL ARRIBA (IZQUIERDA Y DERECHA) - 1 CASILLA /////
+		if ((y_fin = (coll + 1)) && ((x_fin = (row + 1)) || (x_fin = (row - 1))))
+		{
+			if (copia_casillas[x_fin][y_fin]->getOcupada() == false) { //No sé cómo hacer para que no se coman entre sí las piezas blancas :v
+				obstaculo = true;
+				return false;
+			}
+			return true;
+		}
+		break;
+	//Los peones negros solo se mueven hacia abajo y diagonales de abajo derecha/izquierda
+	case 'N':
+		if (y_fin == 6)
+		{
+			////// PRIMER MOVIMIENTO A ESCOGER ENTRE 1-2 CASILLAS HACIA ARRIBA //////
+			if (y_fin <= coll - 2)
+			{
+				// Si hay una pieza en la casilla donde se quiere desplazar y no hay nada
+				if (copia_casillas[row][y_fin]->getOcupada() == false) {
+					obstaculo = true;
+					return false;
+				}
+				return true;
+			}
+		}
+		else
+		{
+			////// RESTO DE MOVIMIENTOS 1 CASILLA HACIA ARRIBA /////
+			if (y_fin = coll - 1)
+			{
+				// Si hay una pieza en la casilla donde se quiere desplazar y no hay nada
+				if (copia_casillas[row][y_fin]->getOcupada() == false) {
+					obstaculo = true;
+					return false;
+				}
+				return true;
+			}
+		}
+
+		////// MOVIMIENTO DIAGONAL ARRIBA (IZQUIERDA Y DERECHA) - 1 CASILLA /////
+		if ((y_fin = (coll - 1)) && ((x_fin = (row + 1)) || (row = (row - 1))))
+		{
+			if (copia_casillas[x_fin][y_fin]->getOcupada() == false) { //No sé cómo hacer para que no se coman entre sí las piezas blancas :v
+				obstaculo = true;
+				return false;
+			}
+			return true;
 		}
 		break;
 	}
