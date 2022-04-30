6c13802efe78106db48fe037955e314bc77ddb0e
ml.salto@alumnos.upm.es <ml.salto@alumnos.upm.es>
2022-04-30 22:13:04 +0200

    CAMBIOS COORDINADOR

--- src/Coordinador.h
+++ src/Coordinador.h
@@ -6,10 +6,13 @@
 class Coordinador
 {
 private:
+	// menu inicial gambito dama
 	Sprite MenuInicial{ "recursos/menu/Menu.png", 0, 0, 130, 80 };
 	Sprite MenuStoryMode{ "recursos/menu/M_StoryMode.png", 0, 0, 130, 80 };
 	Sprite MenuFreePlay{ "recursos/menu/M_FreePlay.png", 0, 0, 130, 80 };
-
+	Sprite MenuOpciones{ "recursos/menu/M_Options.png", 0, 0, 130, 80 };
+	// menu opciones
+	//Sprite Reina{};
 	// coronaciones blancas //
 	Sprite MenuCoronar_B{"recursos/menu/M_CoronacionB.png", 0 , 0, 40 ,60 };
 	Sprite MenuCoronarReina_B{ "recursos/menu/M_CoronacionReinaB.png", 0 , 0,40 ,60 };
@@ -40,6 +43,8 @@ public:
 	// setters //
 	void setMenuInicio(int x);
 	void setMenuCoronacion(int x);
+	void setOpciones(int x);
+
 	// getters //
 	int getEstado();
 	int getModoJuego();
@@ -48,7 +53,11 @@ public:
 protected:
 	Tablero tablero;
 
-	enum Estado { INICIO , JUEGO, PAUSA, AYUDA, FIN}; //editar los estados
+	int tipoJuego; // queensgambit(0), kingsgambit(1)
+
+	int opciones; // Q(0) K(1) Q+H(3) K+H(4) Q+E(5) K+E(6) Q+H+E(7) K+H+E(8)
+
+	enum Estado { INICIO, OPCIONES , JUEGO, PAUSA, AYUDA, FIN}; //editar los estados
 	Estado estado;
 
 	enum EstadoJuego{TURNO, TURNO_NEGRAS, CORONAR_BLANCAS, CORONAR_NEGRAS}; // para gestionar Jugadores
@@ -57,7 +66,7 @@ protected:
 	enum Coronar{ C, REINA, TORRE, ALFIL, CABALLO};
 	Coronar coronar;
 
-    enum Inicio{I ,FREE_PLAY, STORY_MODE, OPCIONES};
+    enum Inicio{I ,FREE_PLAY, STORY_MODE, OPTIONS};
 	Inicio menu_inicio;
 
 	enum Juego{ J , HELP, PAUSE,};
