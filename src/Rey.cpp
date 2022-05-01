9f4750aca9484925ab8a7c234ee111fa9130235a
l.meca <l.meca@alumnos.upm.es>
2022-05-01 14:16:50 +0200

    prueba enroque (sale mal)

--- src/Rey.cpp
+++ src/Rey.cpp
@@ -85,6 +85,58 @@ bool Rey::movimientoLegal(Casilla* fin) {
 	{
 		return true;
 	}
+	//ENROQUES
+//////////// Enroques rey/torre negros ///////
+//enroque corto (a la derecha)
+	if (Tablero::getTipoPiezasT(7, 7) == 2 && Tablero::getTipoPiezasT(4, 7) == 6 && Tablero::getCasillaT(7, 7)->getPieza()->getColorPieza() == color)
+	{
+		for (i = coll + 1; i <= x_fin - 1 && Tablero::getCasillaOcupada(i, y_fin) == false; i++)
+		{
+			if (Tablero::getCasillaOcupada(i, y_fin) == true)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+	//enroque largo (a la izquierda)
+	if (Tablero::getTipoPiezasT(0, 7) == 2 && Tablero::getTipoPiezasT(4, 7) == 6 && Tablero::getCasillaT(0, 7)->getPieza()->getColorPieza() == color)
+	{
+		for (i = coll - 1; i <= x_fin + 1 && Tablero::getCasillaOcupada(i, y_fin) == false; i--)
+		{
+			if (Tablero::getCasillaOcupada(i, y_fin) == true)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+
+	//////////// Enroques rey/torre blancos ///////
+	//enroque corto
+	if (Tablero::getTipoPiezasT(7, 0) == 2 && Tablero::getTipoPiezasT(4, 0) == 6 && Tablero::getCasillaT(7, 0)->getPieza()->getColorPieza() == color)
+	{
+		for (i = coll + 1; i <= x_fin - 1 && Tablero::getCasillaOcupada(i, y_fin) == false; i++)
+		{
+			if (Tablero::getCasillaOcupada(i, y_fin) == true)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+	//enroque largo (a la izquierda)
+	if (Tablero::getTipoPiezasT(0, 0) == 2 && Tablero::getTipoPiezasT(4, 0) == 6 && Tablero::getCasillaT(0, 0)->getPieza()->getColorPieza() == color)
+	{
+		for (i = coll - 1; i <= x_fin + 1 && Tablero::getCasillaOcupada(i, y_fin) == false; i--)
+		{
+			if (Tablero::getCasillaOcupada(i, y_fin) == true)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
 	else {
 		return false;
 	}
