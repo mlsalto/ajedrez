03d6b10f8054b3bdc24926f04468fd4c3c2e9893
ml.salto@alumnos.upm.es <ml.salto@alumnos.upm.es>
2022-05-14 13:34:52 +0200

    nuevos arreglos

--- src/IA1.cpp
+++ src/IA1.cpp
@@ -46,15 +46,161 @@ void IA1::moverPieza(int button, int state, int x, int y)
 						
 						// se puede realizar el movimiento
 						if (pieza->movimientoLegal(Tablero::getCasillaT(x_fin, y_fin)) == TRUE) {
-							
+	
+							// si se come alguna pieza
+							if (Tablero::getTipoPiezasT(x_fin, y_fin) != 0 && Tablero::getCasillaT(x_fin, y_fin)->getPieza()->getColorPieza() != color)
+							{
+								piezacomida = Tablero::getPiezasT(x_fin, y_fin);
+								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
+
+								if (Tablero::detectar_jaque(color) == FALSE) // comprobar que no haya jaque
+								{
+									if (puntos > maximo) {
+										piezaini = pieza; //guardamos valor pieza
+										piezafin = piezacomida;
+										posinix = x_ini; posiniy = y_ini;// guardar valor posicion inicio pieza
+										posfinx = x_fin; posfiny = y_fin; // guardar valor posicion final pieza
+										maximo = puntos;
+										tipomovimiento = 0;
+									}
+								}
+
+								// deshacemos el movimiento
+								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(piezacomida);
+								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(pieza);
+							}
+
+							// si hace enroque
+							else if (Tablero::getTipoPiezasT(x_ini, y_ini) == 6 && (x_fin == 6 || x_fin == 2) && pieza->getPrimerMovimiento() == false && Tablero::getTipoPiezasT(x_fin, y_ini) == 2 && Tablero::getCasillaT(x_fin, y_ini)->getPieza()->getPrimerMovimiento() == false && piezaini->getEnroque() == false)
+							{
+								// hacer enroque
+								if (color == 'N')
+								{
+									if (x_fin == 6) // derecha
+									{
+										piezafin = Tablero::getCasillaT(7, 7)->getPieza(); // torre
+										Tablero::getCasillaT(7, 7)->colocarPieza(0);
+										Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
+										Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+										Tablero::getCasillaT(5, 7)->colocarPieza(piezamovida);
+									}
+
+									if (x_fin == 2) // izquierda
+									{
+										piezafin = Tablero::getCasillaT(0, 7)->getPieza(); // torre
+										Tablero::getCasillaT(0, 7)->colocarPieza(0);
+										Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
+										Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+										Tablero::getCasillaT(3, 7)->colocarPieza(piezamovida);
+									}
+								}
+
+								if (color == 'B')
+								{
+									if (x_fin == 6) // derecha
+									{
+										piezamovida = Tablero::getCasillaT(7, 0)->getPieza(); // torre
+										Tablero::getCasillaT(7, 0)->colocarPieza(0);
+										Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
+										Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+										Tablero::getCasillaT(5, 0)->colocarPieza(piezamovida);
+									}
+
+									if (x_fin == 2) // izquierda
+									{
+										piezamovida = Tablero::getCasillaT(0, 0)->getPieza(); // torre
+										Tablero::getCasillaT(0, 0)->colocarPieza(0);
+										Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
+										Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+										Tablero::getCasillaT(3, 0)->colocarPieza(piezamovida);
+									}
+								}
+
+								// comprobar puntos
+								if (Tablero::detectar_jaque(color) == FALSE) // comprobar que no haya jaque
+								{
+									puntos = getPuntos(color);
+
+									if (puntos > maximo) {
+										piezaini = pieza; //guardamos valor pieza
+										piezafin = piezamovida;
+										posinix = x_ini; posiniy = y_ini;// guardar valor posicion inicio pieza
+										posfinx = x_fin; posfiny = y_fin; // guardar valor posicion final pieza
+										maximo = puntos;
+										tipomovimiento = 1;
+									}
+								}
+
+								// deshacer enroque
+							}
+
+							// si hace en passant
+							else if (color == 'B' && Tablero::getTipoPiezasT(x_ini, y_ini) == 1 && Tablero::getTipoPiezasT(x_fin, y_fin) == 0 && y_fin == y_ini + 1 && y_fin == 5 && (x_fin == x_ini + 1 || x_fin == x_ini - 1) && Tablero::getTipoPiezasT(x_fin, y_fin - 1) == 1 && Tablero::getCasillaT(x_fin, y_fin - 1)->getPieza()->getColorPieza() != color && Tablero::getCasillaT(x_fin, y_fin - 1)->getPieza()->getPassant() == true)
+							{
+								// hace en passant
+								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+								piezacomida = Tablero::getCasillaT(x_fin, y_fin - 1)->getPieza();
+								//Tablero::eliminarPieza(Tablero::getCasillaT(x_fin, y_fin - 1)->getPieza());
+								Tablero::getCasillaT(x_fin, y_fin - 1)->colocarPieza(0);
+
+								// calcular puntos
+								if (Tablero::detectar_jaque(color) == FALSE) // comprobar que no haya jaque
+								{
+									puntos = getPuntos(color);
+
+									if (puntos > maximo) {
+										piezaini = pieza; //guardamos valor pieza
+										piezafin = piezacomida;
+										posinix = x_ini; posiniy = y_ini;// guardar valor posicion inicio pieza
+										posfinx = x_fin; posfiny = y_fin; // guardar valor posicion final pieza
+										maximo = puntos;
+										tipomovimiento = 2;
+									}
+								}
+
+								// deshacer en passant
+								Tablero::getCasillaT(x_fin, y_fin - 1)->colocarPieza(piezacomida);
+								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(pieza);
+							}
+
+							else if (color == 'N' && Tablero::getTipoPiezasT(x_ini, y_ini) == 1 && Tablero::getTipoPiezasT(x_fin, y_fin) == 0  && y_fin == y_ini - 1 && y_fin == 2 && (x_fin == x_ini + 1 || x_fin== x_ini - 1) && Tablero::getTipoPiezasT(x_fin, y_fin + 1) == 1 && Tablero::getCasillaT(x_fin, y_fin + 1)->getPieza()->getColorPieza() != color && Tablero::getCasillaT(x_fin, y_fin + 1)->getPieza()->getPassant() == true)
+							{
+								// hace en passant
+								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
+								piezacomida = Tablero::getCasillaT(x_fin, y_fin + 1)->getPieza();
+								//Tablero::eliminarPieza(Tablero::getCasillaT(x_fin, y_fin + 1)->getPieza());
+								Tablero::getCasillaT(x_fin, y_fin + 1)->colocarPieza(0);
+
+								// calcular puntos
+								if (Tablero::detectar_jaque(color) == FALSE) // comprobar que no haya jaque
+								{
+									puntos = getPuntos(color);
+
+									if (puntos > maximo) {
+										piezaini = pieza; //guardamos valor pieza
+										piezafin = piezacomida;
+										posinix = x_ini; posiniy = y_ini;// guardar valor posicion inicio pieza
+										posfinx = x_fin; posfiny = y_fin; // guardar valor posicion final pieza
+										maximo = puntos;
+										tipomovimiento = 2;
+									}
+								}
+
+								// deshacer en passant
+								Tablero::getCasillaT(x_fin, y_fin + 1)->colocarPieza(piezacomida);
+								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(pieza);
+							}
+
 							// si no come ninguna pieza
-							if (Tablero::getTipoPiezasT(x_fin, y_fin) == 0)
+							else if (Tablero::getTipoPiezasT(x_fin, y_fin) == 0)
 							{
 								// copia del tablero y realizar el movimiento si se mueve a una casilla vacía
 								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
 								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
 
-								if (Tablero::detectar_jaque(color) == FALSE) // comprobar que no haya jaque
+								// calcular puntos
+								if (Tablero::detectar_jaque(color) == FALSE) 
 								{
 									puntos = getPuntos(color);
 
@@ -70,29 +216,6 @@ void IA1::moverPieza(int button, int state, int x, int y)
 								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(0);
 								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(pieza);
 							}
-
-							// si se come alguna pieza
-							if (Tablero::getTipoPiezasT(x_fin, y_fin) != 0 && Tablero::getTipoPiezasT(x_fin, y_fin) != 6)
-							{
-								piezacomida = Tablero::getPiezasT(x_fin, y_fin);
-								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(pieza);
-								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(0);
-
-								if (Tablero::detectar_jaque(color) == FALSE) // comprobar que no haya jaque
-								{
-									if (puntos > maximo) {
-										piezaini = pieza; //guardamos valor pieza
-										piezafin = piezacomida;
-										posinix = x_ini; posiniy = y_ini;// guardar valor posicion inicio pieza
-										posfinx = x_fin; posfiny = y_fin; // guardar valor posicion final pieza
-										maximo = puntos;
-									}
-								}
-
-								// deshacemos el movimiento
-								Tablero::getCasillaT(x_fin, y_fin)->colocarPieza(piezacomida);
-								Tablero::getCasillaT(x_ini, y_ini)->colocarPieza(pieza);
-							}
 						}
 					}
 				}
@@ -102,21 +225,117 @@ void IA1::moverPieza(int button, int state, int x, int y)
 
 		// ****************   REALIZACIÓN DEL MOVIMIENTO DESPUÉS DE DECIDIR QUE ES LO QUE QUIERE HACER   *****************//
 
-		// si mueve a una casilla vacia
-		if (Tablero::getCasillaT(posfinx, posfiny)->getTipoPieza() == 0)
-		{
-			Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
-			Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
-		}
 
 		// si mueve a una casilla ocupada
-		if (Tablero::getCasillaT(posfinx, posfiny)->getTipoPieza() != 0 && Tablero::getCasillaT(posfinx, posfiny)->getTipoPieza() != 6)
+		if (tipomovimiento == 0 )
 		{
 			Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
 			Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
 			Tablero::eliminarPieza(piezafin); //elimina pieza
+
+			// mira si es el primer movimiento de la torre o rey
+			if ((piezaini->getTipoPieza() == 2 || piezaini->getTipoPieza() == 6) && piezaini->getPrimerMovimiento() == false)
+			{
+				piezaini->setPrimerMovimiento(true);
+			}
+
+			if (piezaini->getTipoPieza() == 1)
+			{
+				piezaini->setPassant(false);
+			}
+		}
+		
+		// si hace enroque
+		else if (tipomovimiento == 1)
+		{
+			if (color == 'N')
+			{
+				if (posfinx == 6) // derecha
+				{
+					Tablero::getCasillaT(7, 7)->colocarPieza(0);
+					Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+					Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+					Tablero::getCasillaT(5, 7)->colocarPieza(piezafin);
+				}
+
+				if (posfinx == 2) // izquierda
+				{
+					Tablero::getCasillaT(0, 7)->colocarPieza(0);
+					Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+					Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+					Tablero::getCasillaT(3, 7)->colocarPieza(piezafin);
+				}
+			}
+
+			if (color == 'B') 
+			{
+				if (posfinx == 6) // derecha
+				{
+					Tablero::getCasillaT(7, 0)->colocarPieza(0);
+					Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+					Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+					Tablero::getCasillaT(5, 0)->colocarPieza(piezafin);
+				}
+
+				if (posfinx == 2) // izquierda
+				{
+					Tablero::getCasillaT(0, 0)->colocarPieza(0);
+					Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+					Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+					Tablero::getCasillaT(3, 0)->colocarPieza(piezafin);
+				}
+			}
+
+			piezaini->setEnroque(true);
+		}
+
+		// si hace en passant
+		else if (tipomovimiento == 2)
+		{
+			if (color == 'N')
+			{
+				Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+				Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+				Tablero::getCasillaT(posfinx, posfiny + 1)->colocarPieza(0);
+				Tablero::eliminarPieza(piezafin); //elimina pieza
+			}
+
+			if (color == 'B')
+			{
+				Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+				Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+				Tablero::getCasillaT(posfinx, posfiny - 1)->colocarPieza(0);
+				Tablero::eliminarPieza(piezafin); //elimina pieza
+			}
+		}
+
+		// si mueve a una casilla vacia
+		else if (tipomovimiento == 3 )
+		{
+			Tablero::getCasillaT(posfinx, posfiny)->colocarPieza(piezaini);
+			Tablero::getCasillaT(posinix, posiniy)->colocarPieza(0);
+
+			// mira si es el primer movimiento de la torre o rey
+			if ((piezaini->getTipoPieza() == 2 || piezaini->getTipoPieza() == 6) && piezaini->getPrimerMovimiento() == false)
+			{
+				piezaini->setPrimerMovimiento(true);
+			}
+
+			// mira si es el primer movimiento del peón
+			if ((piezaini->getTipoPieza() == 1))
+			{
+				if (color == 'N')
+				{
+					if (posfiny == 4 && posiniy == 6) piezaini->setPassant(true);
+					else piezaini->setPassant(false);
+				}
+				if (color == 'B')
+				{
+					if (posfiny == 3 && posiniy == 1) piezaini->setPassant(true);
+					else piezaini->setPassant(false);
+				}
+			}
 		}
 
 		turnoterminado = TRUE;
-	
 }
--- src/IA1.h
+++ src/IA1.h
@@ -8,6 +8,9 @@ private:
 	Pieza* pieza; // pieza que movemos
 	Pieza* piezacomida; // pieza que comemos
 	Pieza* piezafin;
+	Pieza* piezamovida;
+
+	int tipomovimiento; // comer pieza (0), enroque (1), passant (2), mover a casilla vacia (3)
 public:
 	IA1();
 	IA1(char colorJugador);
